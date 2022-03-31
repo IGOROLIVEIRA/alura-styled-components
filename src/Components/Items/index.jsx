4fff8f04fa4d0457cced61464264bdaecf00c927
Igor Afonso Oliveira Ruas <igorruas@Igors-MacBook-Pro.local>
2022-03-31 13:34:23 -0300

    finalizando projeto

--- src/Components/Items/index.jsx
+++ src/Components/Items/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import ImageFilter from '../ImageFilter';
 import Item from '../Item';
 
 const Items = styled.div`
@@ -15,8 +16,11 @@ const Items = styled.div`
 
 
 export default (props) => {
-  return <Items>
+  return (
+  <Items>
+    {ImageFilter(props.type)}
     <Item {...props} />
     <span>{props.date}</span>
   </Items>
+  )
 }
